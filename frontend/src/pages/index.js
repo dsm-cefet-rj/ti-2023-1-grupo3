91ffdcbc980ff390be2ca1cb1adaa4e2573f2ea0
DanielDQ <danquintanilha@hotmail.com>
2023-04-11 01:19:40 -0300

    tela-agendamento

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,4 @@
 export * from "./Home";
 export * from "./ProfessionalsMarketplace";
-<<<<<<< HEAD
 export * from "./ProfessionalProfile";
 export * from "./Agendamento";
-=======
-export * from "./ProfessionalProfile";
->>>>>>> 1aff2edc6ced7bc0c834c3d9d3600b3b47166daf
